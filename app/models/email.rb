339c760587d0bdfc0a4f192a0c0ee319ed088d3b
Hamish Hall <hachall@hotmail.com>
2017-09-25 13:42:00 +0100

    small change to email

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,5 +1,5 @@
 class Email < ApplicationRecord
   validates :message, presence: true
-  validates :email, presence: true
+  validates :email_address, presence: true
   validates_email_format_of :email_address
 end
